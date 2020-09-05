5b9326dc7e16b20d40100f8e4f7b5eb6bade5257
jneo8 <jneo8lin@gmail.com>
2020-09-05 20:38:57 +0800

    Auto-deploy at 2020-09-05 20:38:57 CST commit: 08cc653

---
+++ js/dynamicalFile.js
@@ -0,0 +1,23 @@
+function dynamicallyLoadScript(url) {
+    var script = document.createElement("script");
+    script.src = url;
+    document.head.appendChild(script)
+}
+
+function dynamicallyLoadLink(url) {
+    var link = document.createElement("link");
+    link.href = url
+    document.head.appendChild(link)
+}
+
+const cssLinks= [
+]
+
+cssLinks.forEach(url => dynamicallyLoadLink(url))
+
+const urls = [
+    "https://polyfill.io/v3/polyfill.min.js?features=es6",
+    "https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"
+];
+
+urls.forEach(url => dynamicallyLoadScript(url))
